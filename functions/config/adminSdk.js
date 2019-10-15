64d738cbba6371d4e60d918dd1a524babb0267dc
DivyanshBatham <divyansh.sunita@gmail.com>
2019-10-15 17:30:55 +0530

    [Server] Initialized AdminSDK
    - Configured Firebase Admin SDK
    - Added serviceAccountKey.json to .gitignore

--- functions/config/adminSdk.js
+++ functions/config/adminSdk.js
@@ -1,5 +1,9 @@
 var admin = require("firebase-admin");
 
-var adminSdk = admin.initializeApp();
+var serviceAccount = require("./serviceAccountKey.json");
+
+var adminSdk = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 module.exports = adminSdk
