1a4d86406781ab04a498b5445d2249e1f8e93b6a
DivyanshBatham <divyansh.sunita@gmail.com>
2019-10-16 17:01:18 +0530

    [server] Added cors

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const { indexRouter, registerRouter } = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
