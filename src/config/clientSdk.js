d487f87c33a0bd277a2ad53d3c964f596554a42d
DivyanshBatham <divyansh.sunita@gmail.com>
2019-10-16 14:50:00 +0530

    [client] Firebase Initialized

---
+++ src/config/clientSdk.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore'
+import 'firebase/analytics'
+import 'firebase/performance'
+import firebaseConfig from './firebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// From Fireship:
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+firebase.performance();
+firebase.analytics();
+
+// firebase.firestore.settings({ timestampsInSnapshots: true })
+
+export default firebase; 
