d8b5cddc98af2ec18be18ca79d693d36a7973284
DivyanshBatham <divyansh.sunita@gmail.com>
2019-10-20 02:16:51 +0530

    [cleint] Added Styles to Login Page

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { auth } from '../../config/clientSdk';
+import { ConditionalLoader } from '../../components';
+import { Column, Button, Flex } from '../../atoms';
+import { Form, AuthTextField, AuthLink, ErrorText } from './authStyles';
+
+
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
             password: '',
+            loading: false,
+            error: '',
         }
     }
 
@@ -15,45 +23,60 @@ class Login extends Component {
         });
     }
 
-    handleSubmit = async (e) => {
+    handleSubmit = (e) => {
         e.preventDefault();
-        const { email, password } = this.state;
-        console.table({ email, password });
-        try {
-            const res = await auth.signInWithEmailAndPassword(email, password);
-            console.log(res);
-        } catch (err) {
-            console.log(err);
-        }
+        this.setState({
+            loading: true
+        }, async () => {
+            const { email, password } = this.state;
+            try {
+                const res = await auth.signInWithEmailAndPassword(email, password);
+                console.log(res);
+            } catch (err) {
+                console.log(err);
+                this.setState({
+                    loading: false,
+                    error: err.message
+                })
+            }
+        });
     }
 
     render() {
-        const { email, password } = this.state;
+        const { email, password, loading, error } = this.state;
 
         return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        name="email"
-                        aria-label="Email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={this.handleChange}
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        aria-label="Password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={this.handleChange}
-                    />
+            <ConditionalLoader showLoader={loading}>
+                <Column alignItems="center" p="1rem">
+                    <h1>Serve My Customer</h1>
+                    <Form onSubmit={this.handleSubmit}>
+                        <AuthTextField
+                            type="text"
+                            name="email"
+                            aria-label="Email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={this.handleChange}
+                        />
+                        <AuthTextField
+                            type="password"
+                            name="password"
+                            aria-label="Password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={this.handleChange}
+                        />
 
-                    <button type="submit">Login</button>
-                </form>
-            </div>
+                        {error && <ErrorText>* {error}</ErrorText>}
+
+                        <Flex.spaceBetween alignItems="center" mt="2rem">
+                            <AuthLink as={Link} to="/register" fontSize="0.9rem">New user? Create an account</AuthLink>
+                            <Button type="submit">Login</Button>
+                        </Flex.spaceBetween>
+
+                    </Form>
+                </Column>
+            </ConditionalLoader >
         );
     }
 }
