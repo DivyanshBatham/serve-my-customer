2c70db6d3f64037ed3e92358446a3d378cc5dfa1
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-04 02:03:11 +0530

    [server] Firestore Triggers

--- functions/triggers/createCompanyTrigger.js
+++ functions/triggers/createCompanyTrigger.js
@@ -3,17 +3,22 @@ const { firestore } = require('../config/adminSdk');
 
 module.exports = functions.auth.user().onCreate(async (user) => {
     console.log("User Created: ", user);
+    const { admin: isAdmin, companyId } = user.customClaims;
+    // CLAIMS ARE EMPTY HERE 😢
+    // console.log("User Custom Claims: ", user.customClaims);
+    // console.log(isAdmin, companyId);
+    if (isAdmin) {
+        // Create a Company Document:
+        await firestore.collection('companies').doc(user.uid).set({
+            companyName: `${user.displayName}'s Company`,
+        });
+    }
 
-    // Create a Company Document:
-    await firestore.collection('companies').doc(user.uid).set({
-        companyName: `${user.displayName}'s Company`,
-    });
-    
-    // Create Admin Employee Document:
-    return firestore.collection(`companies/${user.uid}/employees`).doc(user.uid).set({
+    // Create Employee Document:
+    return firestore.collection(`companies/${companyId}/employees`).doc(user.uid).set({
         email: user.email,
         name: user.displayName,
-        role: 'admin'
+        role: isAdmin ? 'admin' : 'employee'
     });
 
 });
--- functions/triggers/deleteCompanyTrigger.js
+++ functions/triggers/deleteCompanyTrigger.js
@@ -3,7 +3,8 @@ const { firestore } = require('../config/adminSdk');
 
 module.exports = functions.auth.user().onDelete((user) => {
     console.log("User Deleted: ", user);
-
+    // const { admin: isAdmin, companyId } = user.customClaims;
+    // TODO: What to do when employee is deleted?
     // Delete the Company Document (Only while in development):
     return firestore.collection("companies").doc(user.uid).delete();
 });
