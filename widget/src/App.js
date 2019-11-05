526e07e63775565ce4f0c4fef6ae4dca98b50f8f
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-05 15:57:17 +0530

    [widget] Removed unnecessary files and added parcel
    - Added parcel build script to generate a single outfile on React Widget App
    - Added parcel's dist folder and .cache folder to .gitignore

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Chat Widget
         </a>
       </header>
     </div>
