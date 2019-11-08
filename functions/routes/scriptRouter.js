021276824ec3495c41ec5d17aa1e11b6debc307b
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-08 17:21:21 +0530

    [server] Attaching companyId taken from idToken

--- functions/routes/scriptRouter.js
+++ functions/routes/scriptRouter.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const fs = require('fs');
+const { auth } = require('../config/adminSdk');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+
+    const bearerHeader = req.headers['authorization'];
 
     try {
+        const idToken = bearerHeader.split(' ')[1];
+        const claims = await auth.verifyIdToken(idToken);
+        const { companyId } = claims;
+
         let data = fs.readFileSync('./dist/serve-my-customer.js', 'utf8')
 
-        let output = data.replace('<<<companyId>>>', 'LxfIdcIJAWU00AfIjixX772f19J3');
+        let output = data.replace('<<<companyId>>>', companyId);
 
         res.attachment('serve-my-customer.js');
         res.type('application/javascript');
