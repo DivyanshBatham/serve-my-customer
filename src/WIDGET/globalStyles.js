b9a432561869124966b6bea5755573ebe4509127
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-10 02:02:19 +0530

    [widget] Added global styles for font, using namespacing

--- src/WIDGET/globalStyles.js
+++ src/WIDGET/globalStyles.js
@@ -2,4 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Poppins:400,500|Pacifico&display=swap');
+
+    .serve-my-customer > * {
+        font-family: 'Poppins', sans-serif;
+    }
 `
