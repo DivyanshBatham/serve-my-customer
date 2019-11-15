f68c114cde48be18d8f5332c6c4fb6b5682de399
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-15 17:18:23 +0530

    [widget] Added Theme fetching code

--- src/WIDGET/index.js
+++ src/WIDGET/index.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components'
-import baseTheme from '../theme';
+import themes from '../theme/index';
 import GlobalStyle from './globalStyles';
 import { expandTheme, fetchUserTheme } from '../helpers';
 
 import WidgetApp from './WidgetApp';
 
 const companyId = "<<<companyId>>>";
+const defaultThemeName = "classicBlue"; // Default Theme
 
-fetchUserTheme(companyId).then(userTheme => {
-    const theme = expandTheme(baseTheme, userTheme);
+fetchUserTheme(companyId)
+    .then(({ currentTheme, userTheme, userThemeBase }) => {
+        let theme;
+        if (currentTheme) {
+            if (currentTheme === 'userTheme') {
+                theme = expandTheme(themes[userThemeBase], userTheme);
+            } else {
+                theme = themes[currentTheme];
+            }
+        } else {
+            theme = themes[defaultThemeName];
+        }
 
-    // Getting body:
-    const body = document.querySelector('body');
-    
-    // Creating Serve-My-Customer target:
-    const servemycustomer = document.createElement('div');
-    servemycustomer.className = "serve-my-customer";
-    
-    // Appending Serve-My-Customer target to Body:
-    body.appendChild(servemycustomer);
-    
-    ReactDOM.render(
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <WidgetApp companyId={companyId}/>
-        </ThemeProvider>,
-        servemycustomer
-    );
+        // Getting body:
+        const body = document.querySelector('body');
 
-});
+        // Creating Serve-My-Customer target:
+        const servemycustomer = document.createElement('div');
+        servemycustomer.className = "serve-my-customer";
+
+        // Appending Serve-My-Customer target to Body:
+        body.appendChild(servemycustomer);
+
+        ReactDOM.render(
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <WidgetApp companyId={companyId} />
+            </ThemeProvider>,
+            servemycustomer
+        );
+
+    });
 
 
