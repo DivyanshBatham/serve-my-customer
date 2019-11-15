e99cf7c60e68d23e63e8429a6a88a7ae298c16b6
DivyanshBatham <divyansh.sunita@gmail.com>
2019-11-15 15:57:41 +0530

    [server] Removed Sendgrid

--- functions/config/sendgrid.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-const sgMail = require('@sendgrid/mail');
-
-const SENDGRID_API_KEY = functions.config().sendgrid.key;
-const SENDGRID_INVITATION_TEMPLATE_ID = functions.config().sendgrid.invitationtemplate;
-sgMail.setApiKey(SENDGRID_API_KEY);
-
-module.exports = { sendgrid: sgMail, SENDGRID_INVITATION_TEMPLATE_ID };
